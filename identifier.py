976c39715d98112de237183ff37868af8aba92d2
Kaushik Sivashankar <kaushik.sivashankar@gmail.com>
2021-08-23 16:29:13 +0530

    updated copyright

--- identifier.py
+++ identifier.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
+# hashid.py - Software to identify the different types of hashes
+# Copyright (C) 2013-2015 by c0re <c0re@psypanda.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import io
 import re
 from collections import namedtuple
